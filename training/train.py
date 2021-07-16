b40cea96ab9a2d89c9db3351ae6d8c3d936e5104
Adam Montgomerie <adam@avanssion.com>
2021-07-16 09:19:03 +0900

    refactor args

--- training/train.py
+++ training/train.py
@@ -1,4 +1,3 @@
-import argparse
 import gc
 import os
 import numpy as np
@@ -18,23 +17,23 @@ from transformers import (
 from typing import Mapping
 from sklearn.metrics import mean_squared_error
 
-from .model_with_extra_attention import CLRPModel
+import args
+#from .model_with_extra_attention import CLRPModel
 
 DEVICE = torch.device("cuda")
 
 DEFAULT_CONFIG = {
-    "attention_dropout": 0.2,
+    "attention_dropout": 0.0,
     "batch_size": 8,
     "checkpoint": "roberta-large",
     "early_stopping": True,
     "epochs": 10,
     "eval_steps": 50,
     "eval_style": "epochs",
-    "extra_attention_head": True,
-    "hidden_dropout": 0.2,
+    "extra_attention_head": False,
+    "hidden_dropout": 0.0,
     "learning_rate": 1e-5,
     "max_length": 330,
-    "random_seed": 0,
     "save_path": "output",
     "scheduler": "linear",
     "target_sampling": False,
@@ -92,121 +91,7 @@ class CommonLitDataset(Dataset):
         return np.random.normal(target, std)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--attention_dropout",
-        type=float,
-        default=0.2,
-        help="the dropout rate in self attention layers",
-    )
-    parser.add_argument(
-        "--batch_size", type=int, default=8, help="the number of examples per minibatch"
-    )
-    parser.add_argument(
-        "--checkpoint",
-        type=str,
-        default="roberta-large",
-        help="which pretrained model and tokenizer to use",
-    )
-    parser.add_argument(
-        "--early_stopping", dest="early_stopping", action="store_true"
-    )
-    parser.add_argument(
-        "--epochs", type=int, default=10, help="the number of epochs to run for"
-    )
-    parser.add_argument(
-        "--eval_steps",
-        type=int,
-        default=50,
-        help="how often to evaluate and save the model",
-    )
-    parser.add_argument(
-        "--eval_style",
-        type=str,
-        default="epochs",
-        help="whether to evaluate every epoch or per x number of steps",
-    )
-    parser.add_argument(
-        "--extra_attention_head",
-        dest="extra_attention_head",
-        action="store_true"
-    ),
-    parser.add_argument(
-        "--hidden_dropout",
-        type=float,
-        default=0.2,
-        help="the dropout rate in hidden layers",
-    )
-    parser.add_argument(
-        "--learning_rate", type=float, default=1e-5, help="the max learning rate"
-    )
-    parser.add_argument(
-        "--max_length",
-        type=int,
-        default=330,
-        help="the maximum sequence length in tokens",
-    ),
-    parser.add_argument(
-        "--random_seed",
-        type=int,
-        default=0,
-        help="the random seed",
-    )
-    parser.add_argument(
-        "--save_path",
-        type=str,
-        default="output",
-        help="where to save the trained models",
-    )
-    parser.add_argument(
-        "--scheduler",
-        type=str,
-        default="linear",
-        help="choose a scheduler from [constant, linear, cosine]",
-    )
-    parser.add_argument(
-        "--target_sampling", dest="target_sampling", action="store_true"
-    )
-    parser.add_argument(
-        "--warmup_steps",
-        type=int,
-        default=150,
-        help="the number of warmup steps at the beginning of training",
-    )
-    parser.add_argument(
-        "--weight_decay",
-        type=float,
-        default=0.1,
-        help="the rate of weight decay in AdamW",
-    )
-    parser.set_defaults(target_sampling=False)
-    return parser.parse_args()
-
-
-def build_config(params):
-    return {
-        "attention_dropout": params.attention_dropout,
-        "batch_size": params.batch_size,
-        "checkpoint": params.checkpoint,
-        "early_stopping": params.early_stopping,
-        "epochs": params.epochs,
-        "eval_steps": params.eval_steps,
-        "eval_style": params.eval_style,
-        "extra_attention_head": params.extra_attention_head,
-        "hidden_dropout": params.hidden_dropout,
-        "learning_rate": params.learning_rate,
-        "max_length": params.max_length,
-        "random_seed": params.random_seed,
-        "save_path": params.save_path,
-        "scheduler": params.scheduler,
-        "target_sampling": params.target_sampling,
-        "warmup_steps": params.warmup_steps,
-        "weight_decay": params.weight_decay,
-    }
-
-
-def get_model(checkpoint, hidden_dropout, attention_dropout, extra_attention_head):
+def get_model(checkpoint, hidden_dropout, attention_dropout):
 
     # the naming of the dropout params depends on the model architecture
     if checkpoint.startswith("xlnet") or checkpoint.startswith("transfo-xl"):
@@ -228,53 +113,39 @@ def get_model(checkpoint, hidden_dropout, attention_dropout, extra_attention_hea
             "attention_probs_dropout_prob": attention_dropout,
         }
 
-    # optionally wrap model and add an attention head
-    if extra_attention_head:
-        transformer = AutoModel.from_pretrained(
-            checkpoint,
-            **transformer_config
-        )
-        model = CLRPModel(
-            transformer,
-            hidden_size=transformer.config.hidden_size
-        )
-    else:
-        model = AutoModelForSequenceClassification.from_pretrained(
-            checkpoint,
-            **transformer_config
-        )
-
-    return model.to(DEVICE)
+    return AutoModelForSequenceClassification.from_pretrained(
+        checkpoint,
+        **transformer_config
+    ).to(DEVICE)
 
 
 def train(
     fold: int, train_set: Dataset, valid_set: Dataset, config: Mapping
 ) -> AutoModelForSequenceClassification:
     model = get_model(
-        config["checkpoint"],
-        config["hidden_dropout"],
-        config["attention_dropout"],
-        config["extra_attention_head"]
+        config.checkpoint,
+        config.hidden_dropout,
+        config.attention_dropout,
     )
     model.train()
-    train_loader = DataLoader(train_set, shuffle=True, batch_size=config["batch_size"])
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=config.batch_size)
     criterion = nn.MSELoss()
     optimizer = AdamW(
         model.parameters(),
-        lr=config["learning_rate"],
-        weight_decay=config["weight_decay"],
+        lr=config.learning_rate,
+        weight_decay=config.weight_decay,
     )
     scheduler = get_scheduler(
-        config["scheduler"],
+        config.scheduler,
         optimizer,
-        warmup_steps=config["warmup_steps"],
-        total_steps=len(train_loader) * config["epochs"],
+        warmup_steps=config.warmup_steps,
+        total_steps=len(train_loader) * config.epochs,
     )
 
     best_rmse = 1.0
     total_rmse = 0
 
-    for epoch in range(1, config["epochs"] + 1):
+    for epoch in range(1, config.epochs + 1):
 
         for step, batch in enumerate(train_loader):
             optimizer.zero_grad()
@@ -288,21 +159,21 @@ def train(
 
             saved = False
             if (
-                config["eval_style"] == "steps"
+                config.eval_style == "steps"
                 and current_steps != 0
-                and current_steps % config["eval_steps"] == 0
+                and current_steps % config.eval_steps == 0
             ):
-                train_rmse = total_rmse / config["eval_steps"]
-                valid_rmse = evaluate(model, valid_set, config["batch_size"])
+                train_rmse = total_rmse / config.eval_steps
+                valid_rmse = evaluate(model, valid_set, config.batch_size)
                 total_rmse = 0
 
-                if config["early_stopping"] and valid_rmse < best_rmse:
+                if config.early_stopping and valid_rmse < best_rmse:
                     save(
                         model,
                         train_set.tokenizer,
                         fold,
-                        output_dir=config["save_path"],
-                        extra_attention_head=config["extra_attention_head"]
+                        output_dir=config.save_path,
+                        extra_attention_head=config.extra_attention_head
                     )
                     best_rmse = valid_rmse
                     saved = True
@@ -316,18 +187,18 @@ def train(
                     f"{'Model saved' if saved else ''}"
                 )
 
-        if config["eval_style"] == "epochs":
+        if config.eval_style == "epochs":
             train_rmse = total_rmse / len(train_loader)
-            valid_rmse = evaluate(model, valid_set, config["batch_size"])
+            valid_rmse = evaluate(model, valid_set, config.batch_size)
             total_rmse = 0
 
-            if config["early_stopping"] and valid_rmse < best_rmse:
+            if config.early_stopping and valid_rmse < best_rmse:
                 save(
                     model,
                     train_set.tokenizer,
                     fold,
-                    output_dir=config["save_path"],
-                    extra_attention_head=config["extra_attention_head"]
+                    output_dir=config.save_path,
+                    extra_attention_head=config.extra_attention_head
                 )
                 best_rmse = valid_rmse
                 saved = True
@@ -341,16 +212,16 @@ def train(
                 f"{'Model saved' if saved else ''}"
             )
 
-    if config["eval_style"] == "steps" or not config["early_stopping"]:
-        valid_rmse = evaluate(model, valid_set, config["batch_size"])
+    if config.eval_style == "steps" or not config.early_stopping:
+        valid_rmse = evaluate(model, valid_set, config.batch_size)
         saved = False
         if valid_rmse < best_rmse:
             save(
                 model,
                 train_set.tokenizer,
                 fold,
-                output_dir=config["save_path"],
-                extra_attention_head=config["extra_attention_head"]
+                output_dir=config.save_path,
+                extra_attention_head=config.extra_attention_head
             )
             best_rmse = valid_rmse
             saved = True
@@ -422,33 +293,35 @@ def save(
 
 
 def train_cv(config: Mapping = DEFAULT_CONFIG) -> float:
-    seed_everything(config["random_seed"])
 
-    tokenizer = AutoTokenizer.from_pretrained(config["checkpoint"])
+    if config.random_seed:
+        seed_everything(config.random_seed)
+
+    tokenizer = AutoTokenizer.from_pretrained(config.checkpoint)
     path = os.path.join(os.path.dirname(__file__), "..", "data", "train_folds.csv")
     data = pd.read_csv(path)
 
     if "folds" in config:
-        folds = config["folds"]
+        folds = config.folds
     else:
         folds = data.kfold.unique()
 
     scores = []
 
-    print(f"Training {config['checkpoint']} for {len(folds)} folds in reverse order with:")
+    print(f"Training {config.checkpoint} for {len(folds)} folds in reverse order with:")
     print(config)
 
     for fold in reversed(folds):
         train_set = CommonLitDataset(
             data[data.kfold != fold],
             tokenizer,
-            config["max_length"],
-            sample_targets=config["target_sampling"],
+            config.max_length,
+            sample_targets=config.target_sampling,
         )
         valid_set = CommonLitDataset(
             data[data.kfold == fold],
             tokenizer,
-            config["max_length"],
+            config.max_length,
         )
         trained_model, rmse = train(fold, train_set, valid_set, config)
         scores.append(rmse)
@@ -464,12 +337,9 @@ def seed_everything(seed=0):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    # torch.backends.cudnn.deterministic = True
-    # torch.backends.cudnn.benchmark = True
 
 
 if __name__ == "__main__":
-    params = parse_args()
-    config = build_config(params)
-    cv_score = train_cv(config)
+    params = args.parse_args()
+    cv_score = train_cv(params)
     print(f"CV score: {cv_score}")
